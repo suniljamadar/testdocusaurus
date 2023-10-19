f2720456d781e8fc7390813d3d3a731291f82ae5
sunil jamadar <suniljamadar000@gmail.com>
2023-10-19 21:33:25 +0530

    config changed

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'sunil ghpagse',
+  title: 'sunil ghpages',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
